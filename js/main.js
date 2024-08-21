6c8bb8818264be9edb8c2e063aa934b6cd502443
Agustin Peralta <agusttinp.97@gmail.com>
2024-08-20 22:34:03 -0300

    JSON y storage agregado

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Array de objetos tipo contacto
-const contactos = [{
+const contactos = JSON.parse(localStorage.getItem('contactos')) || [{
     nombre: "Carpi",
     telefono: 1122334455,
     email: "carpi@gmail.com"
@@ -10,6 +10,117 @@ const contactos = [{
     email: "coder@gmail.com"
     }
 ]
+
+
+let principal = document.querySelector(".principal")
+let botones = document.querySelector(".botones")
+let btnMenu = document.createElement("button")
+btnMenu.className = "btn btn-menu"
+btnMenu.innerText = "Menu"
+
+
+//* LISTA DE CONTACTOS
+let btnLista = document.querySelector(".btn-lista")
+btnLista.addEventListener("click", () => {
+    botones.remove()
+    let listaContactos = document.createElement("ul")
+    mostrarContacto(contactos, listaContactos)
+    principal.append(listaContactos)
+    listaContactos.append(btnMenu)
+
+    btnMenu.addEventListener("click", () =>{
+        principal.append(botones)
+        listaContactos.remove()
+    })
+})
+
+//* NUEVO CONTACTO
+let btnNuevoContacto = document.querySelector(".btn-nuevo")
+btnNuevoContacto.addEventListener("click", () => {
+    botones.remove()
+    let formNuevoContacto = document.createElement("form")
+    formNuevoContacto.className = "form-nuevo-contacto"
+    formNuevoContacto.innerHTML = 
+    `
+    <input type="text" class="input-form" id="input-nombre" placeholder="Nombre" required>
+    <input type="text" class="input-form" id="input-telefono" placeholder="Telefono" required>
+    <input type="email" class="input-form" id="input-email" placeholder="Email" required>
+    <input type="submit" class="input-form" id="input-submit" value="Agregar">
+    `
+
+    principal.append(formNuevoContacto)
+    formNuevoContacto.append(btnMenu)
+
+    formNuevoContacto.addEventListener("submit", (e) => {
+        e.preventDefault()
+        contactos.push({nombre: document.querySelector("#input-nombre").value , telefono: document.querySelector("#input-telefono").value, email: document.querySelector("#input-email").value})
+
+        localStorage.setItem('contactos', JSON.stringify(contactos))
+        formNuevoContacto.innerHTML = 
+        `
+        <p>¡Contacto añadido con exito!</p>
+        `
+        formNuevoContacto.append(btnMenu)
+    })
+
+    btnMenu.addEventListener("click", () => {
+        formNuevoContacto.remove()
+        principal.append(botones)
+    })
+})
+
+//* BUSCAR CONTACTO
+let btnBuscar = document.querySelector(".btn-buscar")
+btnBuscar.addEventListener("click", () => {
+    botones.remove()
+    let formBuscar = document.createElement("form")
+    formBuscar.className = "form-buscar"
+    formBuscar.innerHTML = 
+    `
+    <input type="text" class="input-form" id="input-nombre-buscar" placeholder="Nombre" required>
+    <input type="submit" class="input-form" id="input-submit" value="Buscar">
+    `
+    principal.append(formBuscar)
+    formBuscar.append(btnMenu)
+
+    formBuscar.addEventListener("submit", (e) => {
+        e.preventDefault()
+        let nombre = document.querySelector("#input-nombre-buscar").value
+        const contactoFiltrado = contactos.filter(contacto => contacto.nombre === nombre)
+        if(contactoFiltrado.length != 0){
+            mostrarContacto(contactoFiltrado,formBuscar) 
+        }
+    })
+
+    btnMenu.addEventListener("click", () =>{
+        principal.append(botones)
+        formBuscar.remove()
+    })
+})
+
+//* SALIR
+let btnSalir = document.querySelector(".btn-salir")
+btnSalir.addEventListener("click", () => {
+    principal.innerHTML =
+    `
+    <h1> Sesion cerrada </h1>
+    `
+})
+
+//* FUNCIONES
+function mostrarContacto(array, nodoHtml){
+    array.forEach((contacto) => {
+        nodoHtml.innerHTML += 
+        `
+        <li class='item'>
+            <p>Nombre: ${contacto.nombre}</p> 
+            <p>Telefono: ${contacto.telefono}</p> 
+            <p>Email: ${contacto.email}</p>
+        </li>
+        `
+    })
+}
+
 
 // let nombre
 
@@ -56,115 +167,4 @@ const contactos = [{
 //     console.clear()
 //     op = menu()
 // }
-let principal = document.querySelector(".principal")
-let botones = document.querySelector(".botones")
-let btnMenu = document.createElement("button")
-btnMenu.className = "btn btn-menu"
-btnMenu.innerText = "Menu"
-
-
-//* LISTA DE CONTACTOS
-let btnLista = document.querySelector(".btn-lista")
-btnLista.addEventListener("click", () => {
-    botones.remove()
-    let listaContactos = document.createElement("ul")
-    mostrarContacto(contactos, listaContactos)
-    principal.append(listaContactos)
-    listaContactos.append(btnMenu)
-
-    btnMenu.addEventListener("click", () =>{
-        principal.append(botones)
-        listaContactos.remove()
-    })
-})
-
-//* NUEVO CONTACTO
-let btnNuevoContacto = document.querySelector(".btn-nuevo")
-btnNuevoContacto.addEventListener("click", () => {
-    botones.remove()
-    let formNuevoContacto = document.createElement("form")
-    formNuevoContacto.className = "form-nuevo-contacto"
-    formNuevoContacto.innerHTML = 
-    `
-    <input type="text" class="input-form" id="input-nombre" placeholder="Nombre" required>
-    <input type="text" class="input-form" id="input-telefono" placeholder="Telefono" required>
-    <input type="email" class="input-form" id="input-email" placeholder="Email" required>
-    <input type="submit" class="input-form" id="input-submit" value="Agregar">
-    `
-
-    principal.append(formNuevoContacto)
-    formNuevoContacto.append(btnMenu)
-
-    formNuevoContacto.addEventListener("submit", (e) => {
-        e.preventDefault()
-        contactos.push({nombre: document.querySelector("#input-nombre").value , telefono: document.querySelector("#input-telefono").value, email: document.querySelector("#input-email").value})
-
-        formNuevoContacto.innerHTML = 
-        `
-        <p>¡Contacto añadido con exito!</p>
-        `
-        formNuevoContacto.append(btnMenu)
-    })
-
-    btnMenu.addEventListener("click", () => {
-        formNuevoContacto.remove()
-        principal.append(botones)
-    })
-})
-
-//* BUSCAR CONTACTO
-let btnBuscar = document.querySelector(".btn-buscar")
-btnBuscar.addEventListener("click", () => {
-    botones.remove()
-    let formBuscar = document.createElement("form")
-    formBuscar.className = "form-buscar"
-    formBuscar.innerHTML = 
-    `
-    <input type="text" class="input-form" id="input-nombre-buscar" placeholder="Nombre" required>
-    <input type="submit" class="input-form" id="input-submit" value="Buscar">
-    `
-    principal.append(formBuscar)
-    formBuscar.append(btnMenu)
-
-    formBuscar.addEventListener("submit", (e) => {
-        e.preventDefault()
-        let nombre = document.querySelector("#input-nombre-buscar").value
-        const contactoFiltrado = contactos.filter(contacto => contacto.nombre === nombre)
-        if(contactoFiltrado.length != 0){
-            mostrarContacto(contactoFiltrado,formBuscar) 
-        }
-    })
-
-    btnMenu.addEventListener("click", () =>{
-        principal.append(botones)
-        formBuscar.remove()
-    })
-})
-
-//* SALIR
-
-let btnSalir = document.querySelector(".btn-salir")
-btnSalir.addEventListener("click", () => {
-    principal.innerHTML =
-    `
-    <h1> Sesion cerrada </h1>
-    `
-})
-
-//* FUNCIONES
-function mostrarContacto(array, nodoHtml){
-    array.forEach((contacto) => {
-        nodoHtml.innerHTML += 
-        `
-        <li class='item'>
-            <p>Nombre: ${contacto.nombre}</p> 
-            <p>Telefono: ${contacto.telefono}</p> 
-            <p>Email: ${contacto.email}</p>
-        </li>
-        `
-    })
-}
-
-
-
 
